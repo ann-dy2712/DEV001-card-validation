2acbbcfb2a759b614d6d318fdfcd643eb8b570be
ann-dy2712 <113961615+ann-dy2712@users.noreply.github.com>
2022-10-15 01:13:28 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import validator from './validator.js';
 
 
@@ -9,10 +10,67 @@ btnValidate.addEventListener('click', function () {
     const CreditCardNumber = document.getElementById('CreditCardNumber').value;
     validator.isValid(CreditCardNumber);
 
+    let validacion = validator.isValid(CreditCardNumber);
+
+     //Crear condicion que solo permite ingresar valores numericos
+     if (CreditCardNumber==="" || /^[^0-9]*$/.test(CreditCardNumber)) {  
+        alert("Llena el campo Numero de Tarjeta solo con numeros");
+      }else 
+  
+  //Crear alertas pago exitoso y pago rechazado
+  if (validacion) {
+          alert ("Pago exitoso")
+      }
+   else{
+     alert ("Pago rechazado")
+   }
 
 
+    let franquicia = document.querySelector('#logomarca'); 
+    //Crear funcion para mostrar franquicia de tarjeta
+    //function franquicias()
+    //{
+        if (CreditCardNumber [0] == 5){
+        let master = document.createElement("img");
+        master.src = "images/mastercard.png";
+        franquicia.appendChild (master)
+        }
+        else if (CreditCardNumber [0] ==4){
+            franquicia.innerHTML="";
+            let visa = document.createElement("img");
+            visa.src = "images/credit visa.png";
+            franquicia.appendChild (visa);
+        }
+   // }
+   
+   
+    //Function Maskify
+    let inputTarjeta = document.getElementById ("CreditCardNumber").value
+    let numerosMask = validator.maskify(inputTarjeta)
+    document.getElementById("CreditCardNumber").value =numerosMask
+ 
 
+let numero= document.getElementById("CreditCardNumber")
+numero.addEventListener("keyup" ,() => {
+    let numeroinput= document.getElementById("CreditCardNumber").value
+    numero.value = numeroinput
+
+    .replace(/\s/g,"")
+    .replace(/\D/g,"")
+    .trim();
+    let ultimosnumeros= document.getElementById ("ultimoscuatro")
+    ultimosnumeros.textContent= numeroinput.slice(-4)
+    //console.log (ultimosnumeros) 
+})    
+
+//Crear condicion que solo deje ingresar entre 13 y 16 digitos
+if (CreditCardNumber.length < 13|| CreditCardNumber.length > 16){
+    alert("Ingresa entre 13 y 16 caracteres");
+} else{
+    return false;
 }
-)
+    return false;
 
-console.log(validator);
+})
+
+//console.log(validator);
